5a02565cec9709b392bb551bf2bec0cee63d9855
Arnaud Gagnieu <arnaud.gagnieu@hotmail.fr>
2020-11-19 20:41:25 +0100

    creation des fonctions

--- essaicode.py
+++ essaicode.py
@@ -15,32 +15,47 @@ return_book1 = request_site(url)
     
 if return_book1.status_code == 200:
     soup = BeautifulSoup(return_book1.content, 'lxml')
-    book_infos = dict()
+    book_infos1 = dict()
     
     
-    book_infos['titre'] = soup.find('h1').text
-    print(book_infos['titre'])
+    book_infos1['titre'] = soup.find('h1')
+    print(book_infos1['titre'])
 
     
     description = soup.find("p", recursive=False) or ""
     if description:
         description = description.text
-        book_infos["product_description"] = 'description'
+        book_infos1["product_description"] = 'description'
         print(description)
 
 
-    book_infos['url de la page'] = url 
-    print(book_infos['url de la page'])
+    book_infos1['url de la page'] = url 
+    print(book_infos1['url de la page'])
 
 
-    ths = soup.findAll('th')
-    tds = soup.findAll('td')
-    print('caracteristiques')
-    [print(th.text) for th in ths]
-    print('valeurs associees')
-    [print(td.text) for td in tds]
+    def caracteristic_return(target1):
+        
+        th_list = []
+        ths = soup.find("table", {"class": 'table-striped'}).find_all(target1)
+        for th in ths:
+            th_list.append(th.text)
+        print(th_list)
+        return th_list
 
     
+    def value_return(target2):
+    
+        td_list = []
+        tds = soup.find("table", {"class": 'table-striped'}).find_all(target2)
+        for td in tds:
+            td_list.append(td.text)
+        print(td_list)
+        return td_list
+
+    caracteristic_return('th')
+    value_return('td')
+
+
     """def tag_visible(element):
         if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
             return False
@@ -58,13 +73,13 @@ if return_book1.status_code == 200:
 
      
     book_categories = soup.find('ul', {'class': 'breadcrumb'}).findAll('a')[2].get_text()
-    book_infos['catégorie du livre'] = book_categories
-    print('catégorie du livre: \n' + str(book_infos['catégorie du livre']) + '\n')
+    book_infos1['catégorie du livre'] = book_categories
+    print('catégorie du livre: \n' + str(book_infos1['catégorie du livre']) + '\n')
 
-    book_infos['note du livre'] = soup.find('p', {'class': 'star-rating'}).get('class')
-    print('note du livre: \n' + str(book_infos['note du livre']) + '\n')
+    book_infos1['note du livre'] = soup.find('p', {'class': 'star-rating'}).get('class')
+    print('note du livre: \n' + str(book_infos1['note du livre']) + '\n')
 
     img_url = soup.find('img').get('src')
     complete_img_url = urllib.parse.urljoin(url, img_url)
-    book_infos["url de l'image"] = complete_img_url
-    print("Url de l'image: \n" + str(book_infos["url de l'image"]) + '\n')
+    book_infos1["url de l'image"] = complete_img_url
+    print("Url de l'image: \n" + str(book_infos1["url de l'image"]) + '\n')
