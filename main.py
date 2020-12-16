bdab6d363cd4ea1d627d2e9d29b9dfa2b1d495a2
Arnaud Gagnieu <arnaud.gagnieu@hotmail.fr>
2020-12-16 12:59:04 +0100

    final work - loading bar added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+from tqdm import tqdm
 from scraping import Request_site, URLs_scraping, Book_information_scraping, CSV_saving, Pictures_scraping
 
 
@@ -16,16 +17,14 @@ def main(url):
     urls_list = URLs_scraping.get_categories_and_urls(url)[1]
 
     i = 0
-    for category_url in urls_list:
+    for category_url in tqdm(urls_list):
         
         book_link_list = URLs_scraping.get_urls_1_category(category_url)
         list_of_books_information_dicts = Book_information_scraping.books_informations(URL, book_link_list)
         CSV_saving.csv_transfer(list_of_books_information_dicts, category_name[i])
         Pictures_scraping.pictures_scraping(list_of_books_information_dicts, category_name[i])
-        print(category_name[i]+' category scraped')
         i += 1
 
-
-main(URL)
-
-print ('Scraping finished')
+if __name__ == '__main__':
+	main(URL)
+	print ('Scraping finished')
